f95926d8200f1aa1559a05f67cf91e80af6d7656
TzeHong123 <129829470+TzeHong123@users.noreply.github.com>
2023-05-21 12:16:27 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\src\\My project\\barter_it" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\src\\My project\\barter_it"
+  "FLUTTER_ROOT=C:\\src\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\src\\My project\\barter_it\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\src\\My project\\barter_it"
+  "FLUTTER_TARGET=C:\\src\\My project\\barter_it\\..\\Backup\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\src\\My project\\barter_it\\.dart_tool\\package_config.json"
+)
